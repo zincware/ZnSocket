b0721a69194759c4bd7ac9955bc97f86c3beb9c7
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-09-22 16:07:41 +0200

    lint on main

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -1,10 +1,9 @@
-
 import pytest
 
 
 @pytest.mark.parametrize("client", ["znsclient", "redisclient"])
 def test_hmset(client, request):
     c = request.getfixturevalue(client)
-    
+
     c.hmset("name", {"key": "value"})
     assert c.hgetall("name") == {"key": "value"}
