2e5df07aec2ff1b5bef8584f203d1dfa413e4266
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-02-07 09:48:12 +0100

    migrate to uv

--- znsocket/__init__.py
+++ znsocket/__init__.py
@@ -1,5 +1,6 @@
 from znsocket.client import Client
 from znsocket.objects import Dict, List
-from znsocket.server import Server, attach_events
+from znsocket.server import Server, attach_events, Storage
 
-__all__ = ["Client", "Server", "List", "Dict", "attach_events"]
+__all__ = ["Client", "Server", "List", "Dict", "attach_events", "Storage"]
+
