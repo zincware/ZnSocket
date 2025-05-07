49e4595e9fc6a935caab1aeba334c08ab1d5d1b7
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-05-07 11:28:21 +0200

    draft tests

---
+++ tests/test_list_adapter.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+import znsocket
+
+
+
+@pytest.fixture
+def empty() -> None:
+    """Test against Python list implementation"""
+    return None
+
+
+@pytest.mark.parametrize(
+    "client", ["znsclient"] # "znsclient_w_redis", "redisclient", "empty" TODO
+)
+def test_list_adapter_len(client, request):
+    c = request.getfixturevalue(client)
+    key = "list:test"
+    adapter = znsocket.ListAdapter(socket=c, key=key, object=[1, 2, 3, 4])
+    lst = znsocket.List(r=c, key=key)
+    assert len(adapter.object) == 4
+
+    assert len(lst) == len(adapter.object)
+    for idx, value in enumerate(adapter.object):
+        assert lst[idx] == value
