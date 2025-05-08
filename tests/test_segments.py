b465dce5074be1c6f5bb99fdf87c31fc158b2256
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-05-08 21:38:37 +0200

    segments draft

---
+++ tests/test_segments.py
@@ -0,0 +1,61 @@
+from unittest.mock import MagicMock
+
+import numpy as np
+import numpy.testing as npt
+import pytest
+import znjson
+
+import znsocket
+import znsocket.client
+from znsocket.abc import ZnSocketObject
+
+SLEEP_TIME = 0.1
+
+
+@pytest.mark.parametrize(
+    "client", ["znsclient", "znsclient_w_redis", "redisclient"]
+)
+def test_list_extend(client, request):
+    c = request.getfixturevalue(client)
+    lst = znsocket.List(r=c, key="list:test")
+
+    lst.extend(list(range(5)))
+    segments = znsocket.Segments.from_list(lst, "segments:test")
+    assert len(segments) == 5
+
+    segments[2] = "x"
+    assert list(segments) == [0, 1, "x", 3, 4]
+    raw =  segments.get_raw()
+    assert raw[0] == [0, 2, "list:test"]
+    assert raw[1] == [0, 1, "segments:test"]
+    assert raw[2] == [3, 5, "list:test"]
+    assert len(raw) == 3
+
+    segments[0] = "y"
+    assert list(segments) == ["y", 1, "x", 3, 4]
+    raw =  segments.get_raw()
+    assert raw[0] == [1, 2, "segments:test"]
+    assert raw[1] == [1, 2, "list:test"]
+    assert raw[2] == [0, 1, "segments:test"]
+    assert raw[3] == [3, 5, "list:test"]
+    assert len(raw) == 4
+
+    segments[4] = "z"
+    assert list(segments) == ["y", 1, "x", 3, "z"]
+    raw =  segments.get_raw()
+    assert raw[0] == [1, 2, "segments:test"]
+    assert raw[1] == [1, 2, "list:test"]
+    assert raw[2] == [0, 1, "segments:test"]
+    assert raw[3] == [3, 4, "list:test"]
+    assert raw[4] == [2, 3, "segments:test"]
+    assert len(raw) == 5
+
+    # segments[0] = "a"
+    # assert list(segments) == ["a", 1, "x", 3, "z"]
+    # assert raw[0] == [3, 4, "segments:test"]
+    # assert raw[1] == [1, 2, "list:test"]
+    # assert raw[2] == [0, 1, "segments:test"]
+    # assert raw[3] == [3, 4, "list:test"]
+    # assert raw[4] == [2, 3, "segments:test"]
+    # assert len(raw) == 5
+
---
+++ znsocket/objects/segments_obj.py
@@ -0,0 +1,173 @@
+import typing as t
+from collections.abc import MutableSequence
+
+import redis.exceptions
+import znjson
+import json
+
+from znsocket.abc import (
+    ListCallbackTypedDict,
+    ListRepr,
+    RefreshDataTypeDict,
+    RefreshTypeDict,
+    ZnSocketObject,
+)
+from znsocket.client import Client
+from znsocket.exceptions import FrozenStorageError
+from znsocket.utils import decode, encode, handle_error
+
+if t.TYPE_CHECKING:
+    from znsocket import List
+
+
+class Segments(ZnSocketObject, ):  # MutableSequence
+    """Copy of a list object with piece table segments.
+
+    This copy for large objects is used to avoid the need to copy the entire object
+    when a small part of it is changed.
+
+    The data is stored in segments of tubles like:
+    (start, end, data), where start and end are the start and end indices of the part
+    taken from the data list.
+    """
+
+    def __init__(self, r: Client | t.Any, origin: "List", key: str) -> None:
+        self.redis: redis.Redis = r
+        self._origin = origin
+        self._key = key
+
+        # create a new segments list
+        self.redis.lpush(f"segments:{key}", json.dumps((0, len(origin), origin.key)))
+
+    def get_raw(self) -> t.Any:
+        """Get the raw data of the segments list."""
+        # get all segments
+        segments = self.redis.lrange(f"segments:{self._key}", 0, -1)
+        # decode the segments
+        return [json.loads(segment) for segment in segments]
+
+    @classmethod
+    def from_list(cls, origin: "List", key: str) -> "Segments":
+        """Create a Segments object from a list."""
+        r = origin.redis
+        return cls(r, origin, key)
+
+    def __len__(self) -> int:
+        """Return the length of the segments list."""
+        # get all segments and sum their lengths
+        segments = self.redis.lrange(f"segments:{self._key}", 0, -1)
+        length = 0
+        for segment in segments:
+            segment = json.loads(segment)
+            start, end, _ = segment
+            length += end - start
+        return length
+
+    def __getitem__(self, index: int) -> t.Any:
+        """Get an item from the segments list."""
+        from znsocket import List
+
+        single_item = isinstance(index, int)
+        if single_item:
+            index = [index]
+            # TODO: only quqery self.redis.lrange once with the length here as well
+        if isinstance(index, slice):
+            index = list(range(*index.indices(len(self))))
+        # get all segments
+        segments = self.redis.lrange(f"segments:{self._key}", 0, -1)
+        items = []
+        size = 0
+        for segment in segments:
+            segment = json.loads(segment)
+            start, end, target = segment
+            # TODO: converter and stuff
+            lst = List(r=self.redis, key=target)
+            # append to items in range
+            for i in index:
+                if size <= i < end - start + size:
+                    # get the item from the list
+                    items.append(lst[i - size + start])
+            size += end - start
+        if single_item:
+            return items[0]
+        return items
+
+    def __setitem__(self, index: int, value: t.Any) -> None:
+        """Set an item in the segments list."""
+        from znsocket import List
+
+        # list to store the values at
+        lst = List(r=self.redis, key=self._key)
+        # TODO: update the segements, append / insert into the lst
+        # [1, 2, 3, 4] -> |insert, x, 2| [1, 2, X, 3, 4]
+        # (0, 3, target) -> (0, 2, target), (0, 1, new), (3, 4, target)
+
+        lst.append(value)
+
+        # update the segments
+        segments = self.redis.lrange(f"segments:{self._key}", 0, -1)
+        size = 0
+        for idx, segment in enumerate(segments):
+            segment = json.loads(segment)
+            start, end, target = segment
+            shifted_start = start + size
+            shifted_end = end + size
+            # if the index is in the segment, split the segment into two
+            if shifted_start <= index < shifted_end:
+                print(target)
+                if target == self._key:
+                    # if the target is the same as the key, we need to update the segment
+                    # and set the value in the list
+                    lst[index - shifted_start] = value
+                else:
+
+                    self.redis.lset(
+                        f"segments:{self._key}", idx, "__SETITEM_IDENTIFIER__"
+                    )
+                    if start < (index - shifted_start):
+                        self.redis.linsert(
+                            f"segments:{self._key}",
+                            "BEFORE",
+                            "__SETITEM_IDENTIFIER__",
+                            json.dumps((start, index - shifted_start, target)),
+                        )
+                    self.redis.linsert(
+                        f"segments:{self._key}",
+                        "BEFORE",
+                        "__SETITEM_IDENTIFIER__",
+                        json.dumps((len(lst) - 1 , len(lst), self._key)),
+                    )
+                    if index + 1 - shifted_start < end:
+                        self.redis.linsert(
+                            f"segments:{self._key}",
+                            "BEFORE",
+                            "__SETITEM_IDENTIFIER__",
+                            json.dumps((index + 1 - shifted_start, end, target)),
+                        )
+                    self.redis.lrem(
+                        f"segments:{self._key}", 0, "__SETITEM_IDENTIFIER__"
+                    )
+                break
+            # if the index is at the end, replace
+            elif index == size + end - start - 1:
+                self.redis.lset(
+                    f"segments:{self._key}", idx, "__SETITEM_IDENTIFIER__"
+                )
+                self.redis.linsert(
+                    f"segments:{self._key}",
+                    "BEFORE",
+                    "__SETITEM_IDENTIFIER__",
+                    json.dumps((start, index, target)),
+                )
+                self.redis.linsert(
+                    f"segments:{self._key}",
+                    "BEFORE",
+                    "__SETITEM_IDENTIFIER__",
+                    json.dumps((len(lst) - 1 , len(lst), self._key)),
+                )
+                self.redis.lrem(
+                    f"segments:{self._key}", 0, "__SETITEM_IDENTIFIER__"
+                )
+                break
+
+            size += end - start
