e5f82ea7fcb6a7de2c67f5b6a2c1a1e3c6d72272
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-07-11 15:24:09 +0200

    remove claude tests, add pipeline test

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -7,7 +7,7 @@ from znjson.converter import NumpyConverter
 
 import znsocket
 
-
+@pytest.mark.slow
 @pytest.mark.parametrize(
     "client",
     ["znsclient", "znsclient_w_redis", "redisclient"],
@@ -30,7 +30,7 @@ def test_chunking_large_dict_set(client, request, caplog):
     if client in ["znsclient", "znsclient_w_redis"]:
         assert "Splitting message" in caplog.text
 
-
+@pytest.mark.slow
 @pytest.mark.parametrize(
     "client",
     ["znsclient", "znsclient_w_redis", "redisclient"],
@@ -52,3 +52,28 @@ def test_chunking_large_list_append(client, request, caplog):
     npt.assert_array_equal(retrieved_data, [large_array])
     if client in ["znsclient", "znsclient_w_redis"]:
         assert "Splitting message" in caplog.text
+
+@pytest.mark.slow
+@pytest.mark.parametrize(
+    "client",
+    ["znsclient", "znsclient_w_redis", "redisclient"],
+)
+def test_chunking_dict_pipeline(client, request, caplog):
+    """Test that large dictionary updates are chunked automatically."""
+    c = request.getfixturevalue(client)
+
+    pipeline = c.pipeline()
+    data = np.random.rand(1000, 1000)
+    keys = [f"test_large_dict_{idx}" for idx in range(3)]
+    for key in keys:
+        dct = znsocket.Dict(r=pipeline, key=key, converter=[NumpyConverter])
+        dct["data"] = data
+    caplog.set_level("DEBUG", logger="znsocket.client")
+    pipeline.execute()
+    if client in ["znsclient", "znsclient_w_redis"]:
+        assert "Splitting message" in caplog.text
+
+    for key in keys:
+        dct = znsocket.Dict(r=c, key=key, converter=[NumpyConverter])
+        retrieved_data = dct["data"]
+        npt.assert_array_equal(retrieved_data, data)
